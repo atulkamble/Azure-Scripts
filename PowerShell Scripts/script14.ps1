2fff60e77077c7925c5b754fd92db730a8ea438e
Atul Kamble <atul_kamble@hotmail.com>
2024-12-21 12:41:36 +0530

    update

--- PowerShell Scripts/script14.ps1
+++ PowerShell Scripts/script14.ps1
@@ -1,7 +1,7 @@
 $resourceGroupName="staging-grp"
 $location="North Europe"
 $appServicePlan="webplan100203"
-$appServiceName="webapp6677588383"
+$appServiceName="webapp66775883835"
 
 New-AzAppServicePlan -ResourceGroupName $resourceGroupName -Location $location `
 -Name $appServicePlan -Tier "F1"
